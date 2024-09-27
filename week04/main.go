2de0f1f5e10cbc00aded7c831f4c031b79e9fcb4
202444043 <dkstmddyd0406@gmail.com>
2024-09-27 18:42:28 +0900

    v0.6

--- week04/main.go
+++ week04/main.go
@@ -2,8 +2,28 @@
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt" //c language #include <stdio.h>
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	//var i int
+	//i = 55
+
+	//var i int = 55
+
+	var f float64 = 1.92
+
+	//f := 1.92
+	i := "55"
+	fmt.Printf("%f %f\n", f, math.ceil(f))
+	fmt.Println(strings.Title("inha"))
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Println("Hello, world", i)
+	fmt.Print("Hello, world", i, "\n")
+	fmt.Println("Hello, world", i)
+	fmt.Printf("Hello, world %s\n", i)
 }
